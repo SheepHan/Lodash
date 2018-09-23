59f21ad69ff25d0d9b83ea43c8f3149786d7b99c
SheepHan <williamcheng0928@gmail.com>
2018-09-23 10:21:18 +0800

    add

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var _ = require('lodash');
-console.log(_.chunk(['a', 'b', 'c', 'd'], 2))
+function _chunk(array, index) {
+  var result = [];
+  for (var i = 0, len = array.length; i < len; i += index) {
+    result.push(array.slice(i, i + index));
+  }
+
+  console.log(result)
+
+}
+_chunk([1, 2, 3, 4, 5], 2)
